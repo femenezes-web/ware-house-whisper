0dfaae7ea179fe21ec6e5509605f12024d421ce5
femenezes-web <femenezes@gmail.com>
2025-10-24 16:54:30 -0300

    Set base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ware-house-whisper/', // 👈 ADICIONE ISSO
+  base: '/ware-house-whisper/', // 👈 caminho igual ao nome do repositório!
 })
