efb496c205b5df7a397b1052f731b023cb5ff70e
femenezes-web <femenezes@gmail.com>
2025-10-24 16:50:17 -0300

    Update Vite config with base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// 💡 Importe o módulo 'path' do Node.js
-import path from 'path'; 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  base: '/ware-house-whisper/', // 👈 ADICIONE ISSO
+})
