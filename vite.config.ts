bc876b86ead8196772636a4c2954d171ba4b8a43
femenezes-web <femenezes@gmail.com>
2025-10-24 09:50:23 -0300

    Change base path in Vite config to './'

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ware-house-whisper/',
+  // Mude de '/ware-house-whisper/' para './'
+  base: './', 
 })
